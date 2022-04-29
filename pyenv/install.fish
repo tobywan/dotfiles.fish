dfabbc65c934450c09f8356e16bb1498ad2bf8a2
Toby O'Sullivan <spangener@gmail.com>
2022-04-29 09:34:07 +0100

    Pyenv dependencies

--- pyenv/install.fish
+++ pyenv/install.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/env fish
 
 brew install pyenv
+
+# dependencies, see https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+brew install openssl readline sqlite3 xz zlib
+
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
