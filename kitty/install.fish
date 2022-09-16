830d8c59594b82d4124926b72a54ebc36d702abc
Toby O'Sullivan <17815047+tobywan@users.noreply.github.com>
2022-09-16 15:29:55 +0100

    Set up kitty config

--- kitty/install.fish
+++ kitty/install.fish
@@ -1,2 +1,3 @@
 #!/usr/bin/env fish
 curl -sL https://raw.githubusercontent.com/rsaihe/gruvbox-material-kitty/main/colors/gruvbox-material-dark-hard.conf -o $DOTFILES/kitty/gruvbox-material.conf
+curl -sL https://raw.githubusercontent.com/davidmathers/tokyo-night-kitty-theme/trunk/tokyo-night-kitty.conf -o $DOTFILES/kitty/tokyo-night-kitty.conf
